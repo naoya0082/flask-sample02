732d72aa02923a1d9a39687d0d501fcd0a83b8f4
naoya0082 <6ds3f7@gmail.com>
2018-12-01 15:24:42 +0900

    GETとPOST

--- 01.py
+++ 01.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,20 @@ app = Flask(__name__)
 @app.route("/")
 def helloworld():
     return "Hello world"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return '''
+        <form action="/login" method="post">
+            password: <input type="test"><br>
+            <input type="submit">
+        </form>
+        '''
+
+    if request.method == "POST":
+        return "Logged in"
 
 
 if __name__ == "__main__":
